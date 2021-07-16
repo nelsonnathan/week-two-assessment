45af23925da4907c6fbe5dd040994e8dbd26ef2d
Nate Nelson <nathan.nelson99@gmail.com>
2021-07-16 10:29:33 -0600

    adds ASCII art

--- week-two-assessment/js-basics/password.js
+++ week-two-assessment/js-basics/password.js
@@ -7,7 +7,14 @@ const reader = readline.createInterface({
 
 reader.question("Welcome to the password validator! Please input your password:", function(answer) {
     if (answer.length > 9 && answer.includes('1') === true) {
-        console.log("Success! Your password is strong.")
+        console.log(`
+         _________                                       ._.
+        /   _____/__ __   ___   ___  ____   ______ ______| |
+        \\_____ \\ |  |  \\_/___\\/ ___\\/ __ \\ /  ___//  ___/  |
+        /       \\   | /\\  \\__\\  \\__\\   __/ \\___ \\ \\___ \\ \\ |
+       /_______ /____/  \\___   >___  >___  >____  >____  >__
+              \\/             \\/    \\/    \\/     \\/     \\/ \\/
+        `)
     } else {
         console.log('Uh oh! You need to strengthen your password by meeting the 10 character requirement as well as adding the number 1.')
     }
